96e95b604a54244ba09bccfe8c7249019a9f4641
Bahdanovich Yauheni <bahdanovich.yauheni@gmail.com>
2020-03-21 16:39:10 +0300

    init: reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = String(n);
+    n_new = '';
+    if(str[0] == '-'){
+        let str_new = '';
+        for(let i = 1; i < str.length; i++){
+            str_new = str_new + str[i];
+            }
+        str = str_new;
+    }
+    
+    for (let i = str.length-1; i > -1; i--){
+        n_new = n_new + str[i];
+    }
+
+    n_new = Number(n_new);
+    return(n_new);
 }
+
+
+
+
+
+
